b53f41928ea52ae97dd7c525bff470069589496e
dridihaythem <haithemdridiweb@gmail.com>
2022-07-04 00:29:57 +0100

    handle get request

--- app.js
+++ app.js
@@ -3,4 +3,16 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// top-level code is executed only one time.
+// so it will not block the event-loop
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+app.get('/api/v1/tours', (req, res) => {
+	res.status(200).json({
+		status: 'success',
+		results: tours.length,
+		data: { tours },
+	});
+});
+
 app.listen(3000, () => console.log(`Server is listening on port ${port}`));
