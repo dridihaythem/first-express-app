bee938f2f6f8ad3294a3565d567cacc49fb49c3e
dridihaythem <haithemdridiweb@gmail.com>
2022-07-04 17:33:15 +0100

    Create user controller

---
+++ controllers/userController.js
@@ -0,0 +1,15 @@
+exports.getAllUsers = (req, res) => {
+	res.send('get all users');
+};
+exports.getUser = (req, res) => {
+	res.send('get user');
+};
+exports.createUser = (req, res) => {
+	res.send('create user');
+};
+exports.updateUser = (req, res) => {
+	res.send('update user');
+};
+exports.deleteUser = (req, res) => {
+	res.send('delete user');
+};
