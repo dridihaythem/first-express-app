e1baac567d8c313607a70d0c81517b235b232ff6
dridihaythem <haithemdridiweb@gmail.com>
2022-07-04 17:36:33 +0100

    use express.json middleware

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const userRoute = require('./routes/userRoute');
 
+app.use(express.json());
 app.use('/api/v1/users', userRoute);
 
 module.exports = app;
