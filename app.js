a1d31088b671ebb18310b49b19c719dba8487485
dridihaythem <haithemdridiweb@gmail.com>
2022-07-04 06:52:24 +0100

    Serving Static Files

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const userRouter = require('./routes/userRoutes');
 // middleware
 // app.use(morgan('dev'));
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
 	console.log('👋 hello from middleware');
