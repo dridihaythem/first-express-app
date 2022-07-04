76d19c408e633f1a47b9402872b8afaaa8585632
dridihaythem <haithemdridiweb@gmail.com>
2022-07-04 17:40:05 +0100

    Create User

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,18 @@
+const User = require('./../models/userModel');
+
 exports.getAllUsers = (req, res) => {
 	res.send('get all users');
 };
 exports.getUser = (req, res) => {
 	res.send('get user');
 };
-exports.createUser = (req, res) => {
-	res.send('create user');
+exports.createUser = async (req, res) => {
+	try {
+		const user = await User.create(req.body);
+		res.status(500).json({ status: 'success', data: { user } });
+	} catch (e) {
+		res.status(500).json({ status: 'fail', message: e });
+	}
 };
 exports.updateUser = (req, res) => {
 	res.send('update user');
