e5b94bc1a471082892a960318a1955f697a0719b
dridihaythem <haithemdridiweb@gmail.com>
2022-07-05 17:54:05 +0100

    update user

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,7 +7,7 @@ exports.getAllUsers = (req, res) => {
 exports.getUser = async (req, res) => {
 	try {
 		const user = await User.findById(req.params.id);
-		res.status(201).send({ status: 'success', data: { user } });
+		res.status(200).send({ status: 'success', data: { user } });
 	} catch (e) {
 		res.status(500).send({ status: 'fail', message: e.message });
 	}
@@ -22,14 +22,19 @@ exports.createUser = async (req, res) => {
 	}
 };
 
-exports.updateUser = (req, res) => {
-	res.status(200).send({ status: 'success', data: [] });
+exports.updateUser = async (req, res) => {
+	try {
+		const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+		res.status(200).send({ status: 'success', data: { user } });
+	} catch (e) {
+		res.status(404).send({ status: 'fail', message: e.message });
+	}
 };
 
 exports.deleteUser = async (req, res) => {
 	try {
 		const user = await User.findByIdAndDelete(req.params.id);
-		res.status(201).send({ status: 'success', data: null });
+		res.status(200).send({ status: 'success', data: null });
 	} catch (e) {
 		res.status(500).send({ status: 'fail', message: e.message });
 	}
--- readme.md
+++ readme.md
@@ -12,6 +12,16 @@ GET : http://127.0.0.1:3000/api/v1/users/:id
 
 example : http://127.0.0.1:3000/api/v1/users/62c46a29b2ee57a3498854d95
 
+## UPDATE USER :
+
+PATCH : http://127.0.0.1:3000/api/v1/users/:id
+
+body : {"name" : "haythem dridi"}
+
+example : http://127.0.0.1:3000/api/v1/users/62c46a29b2ee57a3498854d9
+
+body : {"name" : "haythem dridi"}
+
 ## DELETE USER :
 
 DELETE : http://127.0.0.1:3000/api/v1/users/:id
