1f3a4bc9a1631e0b04484c91bbea833ea7c046a1
dridihaythem <haithemdridiweb@gmail.com>
2022-07-05 17:41:44 +0100

    create user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,5 @@
+const User = require('./../models/userModel');
+
 exports.getAllUsers = (req, res) => {
 	res.status(200).send({ status: 'success', data: [] });
 };
@@ -6,8 +8,13 @@ exports.getUser = (req, res) => {
 	res.status(200).send({ status: 'success', data: [] });
 };
 
-exports.createUser = (req, res) => {
-	res.status(200).send({ status: 'success', data: [] });
+exports.createUser = async (req, res) => {
+	try {
+		const user = await User.create(req.body);
+		res.status(201).send({ status: 'success', data: { user: user } });
+	} catch (e) {
+		res.status(201).send({ status: 'fail', message: e.message });
+	}
 };
 
 exports.updateUser = (req, res) => {
---
+++ readme.md
@@ -0,0 +1,7 @@
+## CREATE USER :
+
+POST : http://127.0.0.1:3000/api/v1/users
+
+BODY :
+
+{"name" : "haythem","age" : 23,"country" : "tunisia","gender" : "male"}
