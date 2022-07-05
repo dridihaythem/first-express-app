4c0d6db65457cc852ed5190f33f3e8794b75854e
dridihaythem <haithemdridiweb@gmail.com>
2022-07-06 00:25:52 +0100

    Aggregation Pipeline: Matching and Grouping

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -82,7 +82,42 @@ exports.deleteTour = async (req, res) => {
 		});
 	} catch (e) {
 		res.status(404).json({
-			status: 'fai',
+			status: 'fail',
+			message: e,
+		});
+	}
+};
+
+exports.getTourStats = async (req, res) => {
+	try {
+		const stats = await Tour.aggregate([
+			{ $match: { ratingsAverage: { $gte: 4.5 } } },
+			{
+				$group: {
+					_id: { $toUpper: '$difficulty' },
+					numTours: { $sum: 1 },
+					numRatings: { $sum: '$ratingsQuantity' },
+					avgRating: { $avg: '$ratingsAverage' },
+					avgPrice: { $avg: '$price' },
+					minprice: { $min: '$price' },
+					maxprice: { $max: '$price' },
+				},
+			},
+			{
+				$sort: { avgPrice: 1 },
+			},
+			// {
+			// 	$match: { _id: { $ne: 'EASY' } },
+			// },
+		]);
+
+		res.status(200).json({
+			status: 'success',
+			data: { stats },
+		});
+	} catch (e) {
+		res.status(404).json({
+			status: 'fail',
 			message: e,
 		});
 	}
