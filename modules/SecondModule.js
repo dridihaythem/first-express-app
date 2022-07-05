e6d9d37277bf7c99ea5ba4251551d7af46701663
dridihaythem <haithemdridiweb@gmail.com>
2022-07-05 16:48:42 +0100

    int branch

--- modules/SecondModule.js
+++
@@ -1,7 +0,0 @@
-exports.firstFunction = () => {
-	console.log('👋 from first function inside second Module');
-};
-
-exports.SecondFunction = () => {
-	console.log('👋 from second function inside second Module');
-};
