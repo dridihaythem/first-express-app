51ef6e7cacc715fe3e7282f09f709b9114aa42f1
dridihaythem <haithemdridiweb@gmail.com>
2022-07-05 16:54:39 +0100

    create init user controller

---
+++ controllers/userController.js
@@ -0,0 +1,19 @@
+exports.getAllUsers = (req, res) => {
+	res.status(200).send({ status: 'success', data: [] });
+};
+
+exports.getUser = (req, res) => {
+	res.status(200).send({ status: 'success', data: [] });
+};
+
+exports.createUser = (req, res) => {
+	res.status(200).send({ status: 'success', data: [] });
+};
+
+exports.updateUser = (req, res) => {
+	res.status(200).send({ status: 'success', data: [] });
+};
+
+exports.deleteUser = (req, res) => {
+	res.status(200).send({ status: 'success', data: [] });
+};
