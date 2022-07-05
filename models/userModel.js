fe0d2199c853de840bdc9f5a61f8afa5b39418bc
dridihaythem <haithemdridiweb@gmail.com>
2022-07-05 16:52:30 +0100

    create user model

---
+++ models/userModel.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+	name: {
+		type: String,
+		required: [true, 'user must have a name'],
+		minlength: [3, 'name must be at least 3 characters long'],
+		maxlength: 10,
+	},
+	age: {
+		type: Number,
+		required: true,
+	},
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
