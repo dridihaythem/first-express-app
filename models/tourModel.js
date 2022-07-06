3e6ce50853a7ce46bb9d4c2de90e4493108a3afa
dridihaythem <haithemdridiweb@gmail.com>
2022-07-06 17:42:41 +0100

    more Built-In Validators

--- models/tourModel.js
+++ models/tourModel.js
@@ -23,10 +23,16 @@ const tourSchema = new mongoose.Schema(
 		difficulty: {
 			type: String,
 			required: [true, 'A tour must have a difficulty'],
+			enum: {
+				values: ['easy', 'medium', 'difficult'],
+				message: 'Difficulty is either: easy, medium or difficult',
+			},
 		},
 		ratingsAverage: {
 			type: Number,
 			default: 4.5,
+			min: [1, 'Rating must be above 1.0'],
+			max: [5, 'Rating must be below 5.0'],
 		},
 		ratingsQuantity: {
 			type: Number,
