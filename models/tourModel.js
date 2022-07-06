e39eb7bb51be4aea6a0f503a6dee7f80120fea98
dridihaythem <haithemdridiweb@gmail.com>
2022-07-06 17:11:25 +0100

    Document Middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
 	{
@@ -10,6 +11,7 @@ const tourSchema = new mongoose.Schema(
 			maxlength: [40, 'A tour name must have less or equal then 40 characters'],
 			minlength: [10, 'A tour name must have more or equal then 10 characters'],
 		},
+		slug: String,
 		duration: {
 			type: Number,
 			required: [true, 'A tour must have a duration'],
@@ -63,6 +65,17 @@ const tourSchema = new mongoose.Schema(
 		toObject: { virtuals: true },
 	},
 );
+
+// run before save() and create()
+tourSchema.pre('save', function (next) {
+	this.slug = slugify(this.name, { tower: true });
+	next();
+});
+
+tourSchema.post('save', function (doc, next) {
+	console.log(doc);
+	next();
+});
 
 const Tour = mongoose.model('Tour', tourSchema);
 
