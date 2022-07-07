54dbfdd51736a50fbd8d98996dab9f41047b900f
dridihaythem <haithemdridiweb@gmail.com>
2022-07-07 17:08:20 +0100

    Signup  : return token

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,12 @@
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
+const jwt = require('jsonwebtoken');
 
 exports.signup = catchAsync(async (req, res, next) => {
 	const user = await User.create(req.body);
+	const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN });
 	res.status(201).send({
 		status: 'success',
-		data: { user },
+		data: { token, user },
 	});
 });
