093aecfc0c284e26f0542dc3e759b77b02b85cbc
dridihaythem <haithemdridiweb@gmail.com>
2022-07-07 17:17:37 +0100

    Hide Password

--- models/userModel.js
+++ models/userModel.js
@@ -21,6 +21,7 @@ const userSchema = mongoose.Schema({
 		type: String,
 		required: [true, 'Password is required'],
 		minlength: [6, 'Password must be at least 6 characters'],
+		select: false,
 	},
 	passwordConfirm: {
 		type: String,
