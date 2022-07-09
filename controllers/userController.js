a77d3e9d7b28167234305a10f9108f7d3008e683
dridihaythem <haithemdridiweb@gmail.com>
2022-07-09 02:03:25 +0100

    update current user data

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,20 @@
+const User = require('../models/userModel');
+const AppError = require('../utils/AppError');
+const catchAsync = require('./../utils/catchAsync');
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+	if (req.body.password || req.body.correctPassword) {
+		return next(new AppError('this route is not for password updates', 400));
+	}
+	const { name, email } = req.body;
+	const user = await User.findByIdAndUpdate(req.user.id, { name, email }, { new: true, runValidators: true });
+
+	res.status(200).json({
+		status: 'success',
+		data: { user },
+	});
+});
+
 exports.getAllUsers = (req, res) => {
 	res.status(500).json({
 		status: 'error',
