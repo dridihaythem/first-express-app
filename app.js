610d7e7839d17ef95fef6ce16c4eac4d32c9e8a8
dridihaythem <haithemdridiweb@gmail.com>
2022-07-09 02:53:35 +0100

    Data Sanitization

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 const AppError = require('./utils/AppError');
 const globalErrorHandler = require('./controllers/errorController');
 const app = express();
@@ -18,6 +20,12 @@ if (process.env.NODE_ENV === 'development') {
 console.log(process.env.NODE_ENV);
 
 app.use(express.json({ limit: '10kb' }));
+
+//Data Sanitization against NoSQL query injection
+app.use(mongoSanitize());
+//Data Sanitization against XSS
+app.use(xss());
+
 app.use(express.static(`${__dirname}/public`));
 
 const limiter = rateLimit({
