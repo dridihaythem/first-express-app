27e94a69b854cc068db5416e02e9d5205f1ea4f0
dridihaythem <haithemdridiweb@gmail.com>
2022-07-09 18:26:09 +0100

    Creating and Getting Reviews

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const app = express();
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
 
 app.use(helmet());
 
@@ -52,6 +53,7 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/reviews', reviewRouter);
 app.all('*', (req, res, next) => {
 	next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404)); // skip all others middlewares in the stack
 });
---
+++ controllers/reviewController.js
@@ -0,0 +1,24 @@
+const Review = require('./../models/reviewModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.createReview = catchAsync(async (req, res, next) => {
+	const review = await Review.create({
+		review: req.body.review,
+		rating: req.body.rating,
+		tour: req.body.tour,
+		user: req.user._id,
+	});
+	res.status(201).json({
+		status: 'success',
+		data: { review },
+	});
+});
+
+exports.getAllReviews = catchAsync(async (req, res, next) => {
+	const reviews = await Review.find().populate('tour').populate('user');
+	res.status(200).json({
+		status: 'success',
+		results: reviews.length,
+		data: { reviews },
+	});
+});
