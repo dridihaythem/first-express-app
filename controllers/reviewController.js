189f84ad656f2bb1fafc077dc697e89c3753e66a
dridihaythem <haithemdridiweb@gmail.com>
2022-07-10 17:54:48 +0100

    get all reviews

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,25 +1,14 @@
 const Review = require('./../models/reviewModel');
 const catchAsync = require('../utils/catchAsync');
-const { deleteOne, updateOne, createOne, getOne } = require('./handlerfactory');
+const { deleteOne, updateOne, createOne, getOne, getAll } = require('./handlerfactory');
 
 exports.setTourUserIds = (req, res, next) => {
 	if (!req.body.tour) req.body.tour = req.params.id;
 	next();
 };
+
 exports.createReview = createOne(Review);
-
-exports.getAllReviews = catchAsync(async (req, res, next) => {
-	let filter = {};
-	if (req.params.id) filter = { tour: req.params.id };
-
-	const reviews = await Review.find(filter);
-	res.status(200).json({
-		status: 'success',
-		results: reviews.length,
-		data: { reviews },
-	});
-});
-
+exports.getAllReviews = getAll(Review);
 exports.getReview = getOne(Review);
 exports.updateReview = updateOne(Review);
 exports.deleteReview = deleteOne(Review);
