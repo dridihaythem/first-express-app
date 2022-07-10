3b196a3dd7bbb8b903481f022071a785a4101242
dridihaythem <haithemdridiweb@gmail.com>
2022-07-10 17:39:24 +0100

    create global getOne function

--- controllers/handlerfactory.js
+++ controllers/handlerfactory.js
@@ -10,6 +10,20 @@ exports.createOne = (Model) =>
 		});
 	});
 
+exports.getOne = (Model, popOptions) =>
+	catchAsync(async (req, res, next) => {
+		let query = Model.findById(req.params.id);
+		if (popOptions) query = query.populate(popOptions);
+		const doc = await query;
+		if (!doc) {
+			return next(new AppError('No document found with that ID', 404));
+		}
+		res.status(200).json({
+			status: 'success',
+			data: { doc },
+		});
+	});
+
 exports.updateOne = (Model) =>
 	catchAsync(async (req, res, next) => {
 		const doc = await Model.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
