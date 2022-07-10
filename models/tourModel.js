fe76c63a476d062f7392a9bd03bebee4b9bfb245
dridihaythem <haithemdridiweb@gmail.com>
2022-07-10 20:29:03 +0100

    Improving Read Performance with Indexes

--- models/tourModel.js
+++ models/tourModel.js
@@ -115,6 +115,10 @@ const tourSchema = new mongoose.Schema(
 	},
 );
 
+// tourSchema.index({ price: 1 });
+tourSchema.index({ price: 1, ratingsAverage: -1 });
+tourSchema.index({ slug: 1 });
+
 // virtual populate
 tourSchema.virtual('reviews', { ref: 'Review', foreignField: 'tour', localField: '_id' });
 
