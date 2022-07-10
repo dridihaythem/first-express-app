ed5af2b872c09c709017b2a2787bc0b3dc8fd3f5
dridihaythem <haithemdridiweb@gmail.com>
2022-07-10 16:03:38 +0100

    create global update one function

--- controllers/handlerfactory.js
+++ controllers/handlerfactory.js
@@ -1,6 +1,18 @@
 const AppError = require('../utils/AppError');
 const catchAsync = require('../utils/catchAsync');
 
+exports.updateOne = (Model) =>
+	catchAsync(async (req, res, next) => {
+		const doc = await Model.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+		if (!doc) {
+			return next(new AppError('No document found with that ID', 404));
+		}
+		res.status(200).json({
+			status: 'success',
+			data: { data: doc },
+		});
+	});
+
 exports.deleteOne = (Model) =>
 	catchAsync(async (req, res, next) => {
 		const doc = await Model.findByIdAndDelete(req.params.id);
