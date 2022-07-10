f5c82a232763845d10a461651a513101f320aefd
dridihaythem <haithemdridiweb@gmail.com>
2022-07-10 01:52:16 +0100

    Virtual Populate: Tours and Reviews

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -33,7 +33,7 @@ exports.createTour = catchAsync(async (req, res, next) => {
 });
 
 exports.getTour = catchAsync(async (req, res, next) => {
-	const tour = await Tour.findById(req.params.id);
+	const tour = await Tour.findById(req.params.id).populate('reviews');
 	// .populate({ path: 'guides', select: '-__v -passwordChangedAt' });
 	// or
 	// const tour = await Tour.findOne({ _id: req.params.id });
