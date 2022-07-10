b10b41eba0a44ba392bad9a4ad7d5b404c66fa64
dridihaythem <haithemdridiweb@gmail.com>
2022-07-10 16:19:47 +0100

    create review

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,21 +1,12 @@
 const Review = require('./../models/reviewModel');
 const catchAsync = require('../utils/catchAsync');
-const { deleteOne, updateOne } = require('./handlerfactory');
+const { deleteOne, updateOne, createOne } = require('./handlerfactory');
 
-exports.createReview = catchAsync(async (req, res, next) => {
+exports.setTourUserIds = (req, res, next) => {
 	if (!req.body.tour) req.body.tour = req.params.id;
-
-	const review = await Review.create({
-		review: req.body.review,
-		rating: req.body.rating,
-		tour: req.body.tour,
-		user: req.user._id,
-	});
-	res.status(201).json({
-		status: 'success',
-		data: { review },
-	});
-});
+	next();
+};
+exports.createReview = createOne(Review);
 
 exports.getAllReviews = catchAsync(async (req, res, next) => {
 	let filter = {};
--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const { protect, restrictTo } = require('../controllers/authController');
-const { deleteReview, getAllReviews, createReview, updateReview } = require('../controllers/reviewController');
+const {
+	deleteReview,
+	getAllReviews,
+	createReview,
+	updateReview,
+	setTourUserIds,
+} = require('../controllers/reviewController');
 
 const router = express.Router({ mergeParams: true });
 
-router.route('/').get(getAllReviews).post(protect, restrictTo('user'), createReview);
+router.route('/').get(getAllReviews).post(protect, restrictTo('user'), setTourUserIds, createReview);
 router.route('/:id').delete(protect, restrictTo('admin'), deleteReview).patch(updateReview);
 
 module.exports = router;
