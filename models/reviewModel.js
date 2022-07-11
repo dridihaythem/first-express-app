5919dc1b951b2c82ad02eaea5bdb67827f390cff
dridihaythem <haithemdridiweb@gmail.com>
2022-07-11 16:02:34 +0100

    Calculating Average Rating on Tours

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 
 const reviewSchema = mongoose.Schema(
 	{
@@ -46,6 +47,53 @@ reviewSchema.pre(/^find/, function (next) {
 		select: 'name photo',
 	});
 	next();
+});
+
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+	// this point to the current Model
+	const stats = await this.aggregate([
+		{
+			$match: { tour: tourId },
+		},
+		{
+			$group: {
+				_id: '$tour',
+				nRatings: { $sum: 1 },
+				avgRatings: { $avg: '$rating' },
+			},
+		},
+	]);
+	// console.log(stats);
+	if (stats.length > 0) {
+		await Tour.findByIdAndUpdate(tourId, {
+			ratingsQuantity: stats[0].nRatings,
+			ratingsAverage: stats[0].avgRatings,
+		});
+	} else {
+		await Tour.findByIdAndUpdate(tourId, {
+			ratingsQuantity: 0,
+			ratingsAverage: 0,
+		});
+	}
+};
+
+// calculate stats after save new review
+// post middleware doesn't have access to next
+reviewSchema.post('save', function () {
+	// this point to the current document
+	// (this.tour == tour id)
+	this.constructor.calcAverageRatings(this.tour);
+});
+
+// review is deleted or update using
+// findByIdAndUpdate or findByIdAndDelete
+// for these we don't have document middleware
+// but only query middleware (we don't have access to the document)
+// findByIdAnd is a shortcut for findOneAnd
+reviewSchema.post(/^findOneAnd/, function (doc) {
+	if (doc) {
+		doc.constructor.calcAverageRatings(doc.tour);
+	}
 });
 
 const Review = mongoose.model('Review', reviewSchema);
